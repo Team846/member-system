34575d4743649d69000446b73ea2d5297b1a6ffd
Aanand Kainth <akainth015@gmail.com>
2018-06-22 08:44:35 -0700

    Doesn't unnecessarily expend quota.

--- js/index.js
+++ js/index.js
@@ -3,6 +3,14 @@
  * not run any code when it is loaded by the browser.
  */
 
+/**
+ * Create a input field styled with materializecss
+ * @param id The input field's id, also it's firebase key
+ * @param label The input field's label
+ * @param type The input type
+ * @param value The input field
+ * @returns {jQuery}
+ */
 function create$InputField(id, label, type, value) {
     return $('<div class="input-field">')
         .append(
@@ -34,6 +42,8 @@ function createMyProfile(profile) {
     const myProfile = $('#my-profile')
         .empty()
         .append(create$InputField('name', 'Name', 'text', profile.name || currentUser.displayName))
+        .append(create$Select('gender', 'Gender', ['Male', 'Female', 'Other'], 'Male'))
+        .append(create$InputField('birthday', 'Birthday', 'date', profile.birthday))
         .append(create$Select('accountType', 'Account Type', ['Student', 'Parent', 'Mentor'], profile.accountType));
 
     switch (profile.accountType) {
@@ -44,7 +54,8 @@ function createMyProfile(profile) {
                 .append(create$InputField('email', 'Email', 'email', currentUser.email))
                 .append(create$InputField('cellPhone', 'Cell Phone', 'text', profile.cellPhone))
                 .append(create$InputField('github', 'GitHub Username', 'text', profile.github))
-                .append(create$InputField('parent1', 'Parent 1\'s Code', 'text', profile.parent1));
+                .append(create$InputField('parent1', 'Parent 1\'s Code', 'text', profile.parent1))
+                .append(create$InputField('parent2', 'Parent 2\'s Code', 'text', profile.parent2));
             break;
         case 'Parent':
             myProfile
@@ -64,6 +75,19 @@ function createMyProfile(profile) {
                 .append(create$InputField('company', 'Company', 'text', profile.company))
                 .append(create$Select('employment', 'Employment', ['Full time', 'Part time', 'Unemployed'], profile.employment));
     }
+    myProfile
+        .append($('<button class="btn waves-effect">').text('Update').click(() => {
+            M.toast({html: "Updating your profile..."});
+            firebase.firestore().doc(`users/${firebase.auth().currentUser.uid}`).set(profile).then(() => {
+                M.toast({html: "Your profile has been updated"});
+            }).catch((e) => {
+                if (e.code === 'resource-exhausted') {
+                    M.toast({html: 'Try again tomorrow'});
+                } else {
+                    M.toast({html: "Failed to update"});
+                }
+            });
+        }));
     const $accountType = $('#accountType');
     $accountType.formSelect({
         dropdownOptions: {
@@ -74,7 +98,7 @@ function createMyProfile(profile) {
         }
     });
     $('select').not('#accountType').formSelect();
-    $('.input-field').syncWithFireField();
+    $('.input-field').syncWithWindow();
     M.updateTextFields();
 }
 
@@ -133,3 +157,16 @@ String.prototype.copyToClipboard = function() {
         document.getSelection().addRange(selected);
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
