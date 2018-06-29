877a9db7f961a0f587c4172f2a0cf3883615358f
Aanand Kainth <akainth015@gmail.com>
2018-06-28 20:04:49 -0700

    Data transfer order (#4)
    * Started work on members tab
    
    * Show a table listing members of the team who have updated their profile, and a modal for viewing detailed information.
    
    * Fixed HTML injection vulnerability
    
    * Styled parent code button
    
    * Profile works VERY, VERY well
    
    * Showing individual member-profiles

--- js/index.js
+++ js/index.js
@@ -1,171 +1,59 @@
 /*
- * This file in itself should NOT DO ANYTHING. This can only contain functions, and variables. It must
- * not run any code when it is loaded by the browser.
+ * THIS FILE SHOULD NOT RUN ANY CODE!!!!
  */
 
-/**
- * Create a input field styled with materializecss
- * @param id The input field's id, also it's firebase key
- * @param label The input field's label
- * @param type The input type
- * @param value The input field
- * @returns {jQuery}
- */
-function create$InputField(id, label, type, value) {
-    return $('<div class="input-field">')
-        .append(
-            $('<input>')
-                .val(value)
-                .attr('id', id)
-                .attr('type', type))
-        .append(
-            $('<label>')
-                .text(label))
-}
-
-function create$Select(id, label, options, selected) {
-    const $select = $('<select class="yellow">')
-        .attr('id', id);
-    options.map(it => $('<option>').text(it)).forEach($it => {
-        if ($it.text() === selected) {
-            $it.attr('selected', true);
-        }
-        $it.appendTo($select);
-    });
-    return $('<div class="black-text input-field">')
-        .append($select)
-        .append($('<label>').text(label).attr('for', id));
-}
-
-function createMyProfile(profile) {
-    const currentUser = firebase.auth().currentUser;
-    const myProfile = $('#my-profile')
-        .empty()
-        .append(create$InputField('name', 'Name', 'text', profile.name || currentUser.displayName))
-        .append(create$Select('gender', 'Gender', ['Male', 'Female', 'Other'], profile.gender))
-        .append(create$InputField('birthday', 'Birthday', 'date', profile.birthday))
-        .append(create$Select('accountType', 'Account Type', ['Student', 'Parent', 'Mentor'], profile.accountType));
-
-    switch (profile.accountType) {
-        case 'Student':
-            const year = new Date().getFullYear();
-            myProfile
-                .append(create$Select('graduationYear', 'Graduation Year', [...Array(5)].map((_, i) => year + i), year))
-                .append(create$InputField('email', 'Email', 'email', currentUser.email))
-                .append(create$InputField('cellPhone', 'Cell Phone', 'text', profile.cellPhone))
-                .append(create$InputField('github', 'GitHub Username', 'text', profile.github))
-                .append(create$InputField('parent1', 'Parent 1\'s Code', 'text', profile.parent1))
-                .append(create$InputField('parent2', 'Parent 2\'s Code', 'text', profile.parent2));
-            break;
-        case 'Parent':
-            myProfile
-                .append($('<button' +
-                    ' class="btn waves-button-input"' +
-                    ' onclick="`${firebase.auth().currentUser.uid}`.copyToClipboard();' +
-                    'M.toast({html: `Copied to clipboard`})">')
-                    .text('Get Parent Code'));
-        // WE WANT TO FALLTHROUGH!!!
-        case 'Mentor':
-            myProfile
-                .append(create$InputField('homePhone', 'Home Phone', 'text', profile.homePhone))
-                .append(create$InputField('cellPhone', 'Cell Phone', 'text', profile.cellPhone))
-                .append(create$InputField('address', 'Address', 'text', profile.address))
-                .append(create$InputField('city', 'City', 'text', profile.city))
-                .append(create$InputField('zipCode', 'Zip Code', 'text', profile.zipCode))
-                .append(create$InputField('company', 'Company', 'text', profile.company))
-                .append(create$Select('employment', 'Employment', ['Full time', 'Part time', 'Unemployed'], profile.employment));
-    }
-    myProfile
-        .append($('<button class="btn waves-effect">').text('Update').click(() => {
-            M.toast({html: "Updating your profile..."});
-            collectAndSendProfileInfo();
-        }));
-    const $accountType = $('#accountType');
-    $accountType['formSelect']({
-        dropdownOptions: {
-            onCloseEnd: function() {
-                profile.accountType = M.FormSelect.getInstance($accountType[0]).input.value;
-                createMyProfile(profile);
-            }
-        }
-    });
-    $('select').not('#accountType')['formSelect']();
-    M.updateTextFields();
-}
-async function getUserProfile(uid) {
-    if (firebase.auth().currentUser === null) {
-        throw new Error("Attempted to get current user's profile without a user logged in");
-    }
-    return (await firebase.firestore().doc(uid || `users/${firebase.auth().currentUser.uid}`).get()).data();
-}
-
-function collectAndSendProfileInfo() {
-    const profile = {};
-    const $myProfile = $('#my-profile');
-    $myProfile
-        .find('select')
-        .each((i, select) => {
-            profile[select.id] = M.FormSelect['getInstance'](select).input.value
-        });
-    $myProfile
-        .find('input')
-        .not('.select-dropdown')
-        .each((i, input) => {
-            profile[input.id] = input.value;
-        });
-    firebase.firestore().doc(`users/${firebase.auth().currentUser.uid}`).set(profile).then(() => {
-        M.toast({html: "Your profile has been updated"});
-    }).catch((e) => {
-        if (e.code === 'resource-exhausted') {
-            M.toast({html: 'Try again tomorrow'});
-        } else {
-            M.toast({html: "Failed to update"});
-        }
-        console.error(e);
-    });
-}
-
-function openSignInPopup() {
-    firebase.auth()['signInWithPopup'](new firebase.auth['GoogleAuthProvider']());
-}
-
-function showExtendedNavbar(show) {
+function showExtendedNavbar(shouldShowNavbar) {
     const $navContent = $('.nav-content');
-    const $fixedNav = $('.navbar-fixed');
-    const paddingTop = 64;
-    switch (show) {
-        case null:
-        case undefined:
-        case true:
-            $navContent.slideDown();
-            $fixedNav['animate']({
-                height: 112 + paddingTop
+    const $navContainer = $('.navbar-fixed');
+    const padding = 32;
+
+    return new Promise(function(resolve, reject) {
+        switch (shouldShowNavbar) {
+            case undefined:
+            case null:
+            case true:
+                $navContent.slideDown();
+                $navContainer.animate({
+                    height: 110 + padding
+                }, resolve);
+                break;
+            default:
+                $navContent.slideUp();
+                $navContainer.animate({
+                    height: 64 + padding
+                }, resolve);
+        }
+    });
+}
+
+function switchToPage(pageNumber) {
+    return new Promise(function(resolve, reject) {
+        $('.page').each((i, page) => {
+            $(page).animate({
+                left: `${(i - pageNumber) * 100}vw`
+            }, resolve);
+            if (pageNumber !== '0') {
+                document.body.style.overflowY = 'hidden';
+            } else {
+                document.body.style.overflowY = 'scroll';
+            }
+        });
+    });
+}
+
+function setupSpecificProfile(value) {
+    const $adultProfile = $('#adult-profile');
+    const $student = $('#student-profile');
+    switch (value) {
+        case 'Student':
+            $adultProfile.fadeOut(() => {
+                $student.fadeIn();
             });
             break;
-        default:
-            $navContent.slideUp();
-            $fixedNav['animate']({
-                height: 64 + paddingTop
+        case 'Adult':
+            $student.fadeOut(() => {
+                $adultProfile.fadeIn();
             });
             break;
     }
 }
-
-String.prototype.copyToClipboard = function() {
-    const textarea = $('<textarea>')
-        .val(this)
-        .attr('readonly', true)
-        .css('position', 'absolute')
-        .css('left', '-9999px')
-        .appendTo($(document.body))[0];
-    const selected = document.getSelection().rangeCount > 0
-        ? document.getSelection().getRangeAt(0)
-        : false;
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
-    if (selected) {
-        document.getSelection().removeAllRanges();
-        document.getSelection().addRange(selected);
-    }
-};
--- js/initialize.js
+++ js/initialize.js
@@ -1,31 +1,165 @@
 /*
  * This is the file that DOES stuff, as in adding listeners for events.
  */
-const $authToggle = $('#auth-toggle').click(() => {
+// switchToPage(0);
+
+firebase.auth()['onAuthStateChanged'](async (user) => {
+    window.user = user;
+    const $progress = $('#progress');
+    switch (user) {
+        case null:
+        case undefined:
+            $authToggle.text('Sign In');
+            await showExtendedNavbar(false); // Allow the animation to complete
+            $progress.fadeOut();
+            break;
+        default:
+            $authToggle.text('Sign Out');
+            window.users = firebase.firestore().collection('users').get();
+            await showExtendedNavbar();
+            $progress.fadeIn();
+            $('#name').val(user.displayName);
+            $('#uid').val(user.uid);
+            $('#email').val(user.email);
+            $('#photo-url').val(user.photoURL);
+            window.$myProfile = $('#my-profile').submit(e => {
+                e.preventDefault(); // Stop the page from reloading
+                const profile = {};
+                $myProfile
+                    .find('select')
+                    .each((i, element) =>
+                        profile[element.id] = M.FormSelect.getInstance(element).input.value);
+                $myProfile
+                    .find('input')
+                    .not('.select-dropdown') // Material selects create an input to display the current selection
+                    .not('[type=submit]') // Filter out the "Update" button
+                    .each((i, element) => {
+                        profile[element.id] = element.value
+                    });
+                M.toast({html: "Updating Profile..."});
+                firebase.firestore().doc(`users/${user.uid}`).set(profile)
+                    .then(() => M.toast({html: "Updated successfully"}))
+                    .catch(e => {
+                        console.error(e);
+                        M.toast({html: "Failed to updated"});
+                    });
+            });
+            M.updateTextFields();
+            window.users.then(collection => {
+                $progress.fadeOut();
+                collection.forEach(doc => {
+                    const profile = doc.data();
+                    if (profile.uid === user.uid) {
+                        setupSpecificProfile(profile['account-type']);
+                        try {
+                            Object.keys(profile).forEach(key => {
+                                const $input = $(`#${key}`);
+                                const inputElement = $input[0];
+                                if (inputElement instanceof HTMLSelectElement) {
+                                    M.FormSelect.getInstance(inputElement).input.value = profile[key];
+                                } else {
+                                    $input.val(profile[key]);
+                                }
+                            });
+                        } catch (e) {
+                        }
+                    }
+                    let row = $('<tr>');
+                    for (let i = 0; i < columns; i++) {
+                        row.append($('<td>').text(profile[tableOrder[i]]).width(columnWidth));
+                    }
+                    row.click(e => {
+                        const tbody = $('<tbody>');
+                        Object.keys(profile).forEach(key => {
+                            if (key === 'uid' || key === 'photo-url') return;
+                            tbody.append($('<tr>')
+                                .append($('<td>').text(key.replace(/-/g, ' ').toTitleCase()))
+                                .append($('<td>').text(profile[key])))
+                        });
+
+                        $('#profile-modal')
+                            .empty()
+                            .append($(`<img class="left" src="${profile['photo-url']}">`)
+                                .width(128)
+                                .height(128)
+                                .css('margin-right', '32px')
+                                .css('border-radius', '50%'))
+                            .append($('<h3>').text(profile['name']))
+                            .append($('<h4>').text(profile['email']))
+                            .append($('<table>').append(tbody))
+                        .modal('open');
+                    });
+                    $('#member-table').find('tbody').append(row);
+                });
+                M.updateTextFields();
+            });
+            break;
+    }
+});
+
+window.$authToggle = $('#auth-toggle').click(() => {
     switch (firebase.auth().currentUser) {
         case null:
         case undefined:
-            openSignInPopup();
+            firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
             break;
         default:
             firebase.auth().signOut();
             break;
     }
+}); // Setup login and signing out
+
+$('#profile-modal').modal(); // Setup the user profile modal
+
+window.$subpageLinks = $('.subpage-link').click(async (e) => {
+    const page = e.target.getAttribute('data-page');
+    await switchToPage(page);
+    $subpageLinks.removeClass("active").filter(`[data-page=${page}]`).addClass('active');
 });
 
-firebase.auth().onAuthStateChanged(user => {
-    switch (user) {
-        case null:
-        case undefined:
-            $authToggle.text('Sign In');
-            showExtendedNavbar(false);
-            break;
-        default:
-            $authToggle.text('Sign Out');
-            showExtendedNavbar();
-            getUserProfile().then(profile => {
-                createMyProfile(profile || {});
-            });
-            break;
+window.$graduationYear = $('#graduation-year').empty();
+
+[...Array(5)].forEach((_, i) => {
+    const year = new Date().getFullYear() + i;
+    $graduationYear.append(`<option value="${year}">${year}</option>`);
+});
+
+$('select').formSelect(); // Initialize the select elements
+$('#account-type').formSelect({
+    dropdownOptions: {
+        onCloseEnd: () => {
+            setTimeout(() => {
+                setupSpecificProfile(M.FormSelect.getInstance($('#account-type')[0]).input.value);
+            }, 100);
+        }
+    }
+}); // It's a special dropdown
+
+let ticking = false;
+addEventListener('scroll', e => {
+    ticking = true;
+    if (ticking) {
+        requestAnimationFrame(() => {
+            $('#member-table').find('thead')
+                .css('fixed');
+            ticking = false;
+        });
     }
 });
+
+String.prototype.toTitleCase = function() {
+    const str = this.toLowerCase().split(' ');
+    for (let i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+};
+
+window.columns = Math.min(Math.floor((innerWidth - 64) / 200), 9); // Oooh big boy math, columns should be over 200px
+window.columnWidth = Math.floor(innerWidth - 64) / columns;
+window.tableOrder = ['name', 'email', 'cell-phone', 'home-phone', 'preferred-contact', 'address'];
+const row = $('<tr>');
+for (let i = 0; i < columns; i++) {
+    row.append($('<th>').text(tableOrder[i].replace('-', ' ').toTitleCase()).width(columnWidth));
+}
+$('#member-table').find('thead').append(row);
