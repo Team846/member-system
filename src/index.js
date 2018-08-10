a4b1503ebde6e60af396d9e29d0c65ec1a26a763
Aanand Kainth <akainth015@gmail.com>
2018-08-10 15:25:28 -0700

    Added production configuration

--- src/index.js
+++ src/index.js
@@ -17,12 +17,12 @@ const configurations = {
         messagingSenderId: "517864814039"
     },
     production: {
-        apiKey: "AIzaSyA_1zO62l-Vba_eLyOFXFNn4oukLLQmijM",
-        authDomain: "staged-member-system.firebaseapp.com",
-        databaseURL: "https://staged-member-system.firebaseio.com",
-        projectId: "staged-member-system",
-        storageBucket: "staged-member-system.appspot.com",
-        messagingSenderId: "517864814039"
+        apiKey: "AIzaSyBaTjYf7uhPfgKO6N0TYw1M8MHTHDO5iw0",
+        authDomain: "official-member-system.firebaseapp.com",
+        databaseURL: "https://official-member-system.firebaseio.com",
+        projectId: "official-member-system",
+        storageBucket: "official-member-system.appspot.com",
+        messagingSenderId: "682019464677"
     }
 };
 
