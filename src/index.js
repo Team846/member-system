7739a9b0b24b1c57c66c2947a384e79fbb665163
Aanand Kainth <akainth015@gmail.com>
2018-09-20 21:28:01 -0700

    Production Firebase configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker, {isLocalhost} from './registerServiceWorker';
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
@@ -14,10 +14,18 @@ const configurations = {
         projectId: "staged-member-system-2",
         storageBucket: "staged-member-system-2.appspot.com",
         messagingSenderId: "263064195037"
+    },
+    production: {
+        apiKey: "AIzaSyAcx0K0EcVt9mycKFmMhAC0wXA5ZemPoKY",
+        authDomain: "official-member-system-99805.firebaseapp.com",
+        databaseURL: "https://official-member-system-99805.firebaseio.com",
+        projectId: "official-member-system-99805",
+        storageBucket: "official-member-system-99805.appspot.com",
+        messagingSenderId: "662488231065"
     }
 };
 
-firebase.initializeApp(configurations.development);
+firebase.initializeApp(isLocalhost ? configurations.development : configurations.production);
 
 firebase.firestore().settings({
     timestampsInSnapshots: true
